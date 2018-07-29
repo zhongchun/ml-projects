f3c70fd1af654148b0902ff75aefb62de5b258ca
zhongchun <zhongchunyu@gmail.com>
2018-07-29 11:52:24 +0800

    add house price predictor

--- house_price_predictor/data_insight.py
+++ house_price_predictor/data_insight.py
@@ -11,13 +11,40 @@
 
 import pandas as pd
 
-train_url = "./house_price_predictor/datasets/train.csv"
-train_df = pd.read_csv(train_url)
+path = "./house_price_predictor/datasets/"
+train_url = f'{path}train.csv'
+test_url = f'{path}test.csv'
 
+# train_df = pd.read_csv(train_url)
+train_df = pd.read_csv(train_url, index_col='Id')
+test_df = pd.read_csv(test_url, index_col='Id')
+
+# 1. Preview the data and take a peek
+print("=" * 120)
 print(train_df.head(10))
+print("=" * 120)
+print(train_df.tail(10))
+print("=" * 120)
+print(train_df.columns.values)
+print("=" * 120)
 print(train_df.shape)
+print("=" * 120)
+train_df.info()
+print("=" * 120)
 print(train_df.describe())
+print("=" * 120)
 # show the descriptive statistics of object data types
 print(train_df.describe(include=['O']))
-train_df.info()
+print("=" * 120)
 print(train_df.isnull().sum())
+print("=" * 120)
+
+# proportion of null values, i.e. missing values
+total = train_df.isnull().sum().sort_values(ascending=False)
+percent_1 = train_df.isnull().sum() / train_df.isnull().count() * 100
+percent_2 = (round(percent_1, 2)).sort_values(ascending=False)
+missing_data = pd.concat([total, percent_2], axis=1, keys=['Total', '%'])
+print(total)
+print("=" * 40)
+print(missing_data.head(20))
+print("=" * 40)
---
+++ house_price_predictor/predictor.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ @FileName: undefined
+ @Desc:
+ @Author: yuzhongchun
+ @Date: 2018-07-24 23:04:30
+ @Last Modified by: yuzhongchun
+ @Last Modified time: 2018-07-24 23:04:30
+"""
+
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+path = "./house_price_predictor/datasets/"
+train_url = f'{path}train.csv'
+test_url = f'{path}test.csv'
+
+# train_df = pd.read_csv(train_url)
+train_df = pd.read_csv(train_url, index_col='Id')
+test_df = pd.read_csv(test_url, index_col='Id')
+
+# 2. Preprocess the data
+target = train_df['SalePrice']
+train_df = train_df.drop('SalePrice', axis=1)
+train_df['training_set'] = True
+test_df['training_set'] = False
+
+full_df = pd.concat([train_df, test_df])
+# print(full_df.shape)
+
+# fill in the missing values
+full_df = full_df.interpolate()
+# Convert categorical variable into dummy/indicator variables
+full_df = pd.get_dummies(full_df)
+
+train_df = full_df[full_df['training_set'].__eq__(True)]
+train_df = train_df.drop('training_set', axis=1)
+
+test_df = full_df[full_df['training_set'].__eq__(False)]
+test_df = test_df.drop('training_set', axis=1)
+
+rf = RandomForestRegressor(n_estimators=100, n_jobs=-1)
+rf.fit(train_df, target)
+
+preds = rf.predict(test_df)
+submission = pd.DataFrame({'Id': test_df.index, 'SalePrice': preds})
+submission.to_csv(f'{path}submission.csv', index=False)
+print("Done")
